9a4f5defca373fa2dbaa4c846351c1f9dcf04981
anmol26s <github@datamol.org>
2018-09-02 02:32:28 +0530

    symfony fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -95,6 +95,7 @@ init_composer() {
     || ynh_die "Unable to install Composer"
 
   # update dependencies to create composer.lock
+  exec_composer "$DESTDIR" update symfony/flex --no-plugins
   exec_composer "$DESTDIR" install  --no-dev --optimize-autoloader  \
     || ynh_die "Unable to update Kimai2 core dependencies"
 }
