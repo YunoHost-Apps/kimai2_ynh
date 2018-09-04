2d068f8a3b2ace8dae6d84fc9c34fb1e7797a292
anmol26s <github@datamol.org>
2018-09-05 03:11:19 +0530

    symfony-cmd: not found fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -116,7 +116,7 @@ init_composer_update() {
     || ynh_die "Unable to install Composer"
 
   # update dependencies to create composer.lock
-  exec_composer "$DESTDIR" update symfony/flex --no-plugins
+  exec_composer "$DESTDIR" update symfony/flex --no-plugins --no-scripts
   exec_composer "$DESTDIR" install  --no-dev --optimize-autoloader \
     || ynh_die "Unable to update Kimai2 core dependencies"
 }
