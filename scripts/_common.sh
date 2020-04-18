bf7ffa001c4bae190a496b6a792ea1cb9b90580b
yalh76 <yalh@yahoo.com>
2020-04-18 17:59:42 +0200

    Moving to git release

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,8 @@
 # dependencies used by the app
 pkg_dependencies=""
 
-extra_pkg_dependencies="php7.3-mbstring php7.3-gd php7.3-intl php7.3-pdo php7.3-xml php7.3-zip php7.3-xsl php7.3-ldap php7.3-mysql php7.3-sqlite3 php7.3-ctype php7.3-iconv php7.3-json php7.3-cli php7.3-simplexml php7.3-tokenizer"
+extra_pkg_dependencies="php7.3-gd php7.3-intl php7.3-json php7.3-mbstring php7.3-pdo php7.3-zip php7.3-xml php7.3-xsl php7.3-ldap php7.3-mysql php7.3-sqlite3"
+#  php7.3-ctype php7.3-iconv php7.3-cli php7.3-simplexml php7.3-tokenizer php7.3-common php7.3-curl
 
 #=================================================
 # PERSONAL HELPERS
