a80da800d3e06de87132469cbc35467c6d98f7f7
yalh76 <yalh@yahoo.com>
2020-11-01 13:53:49 +0100

    switching to composer 2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,6 +9,8 @@ pkg_dependencies=""
 
 YNH_PHP_VERSION="7.3"
 
+YNH_COMPOSER_VERSION="2.0.4"
+
 extra_php_dependencies="php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-pdo php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-xsl php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-sqlite3"
 
 #=================================================
