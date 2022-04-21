a92d35e1db4acd08cdffc632733b1a0aea783880
ericgaspar <junk.eg@free.fr>
2022-04-21 11:00:40 +0200

    1.19.4

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 YNH_PHP_VERSION="8.0"
 
-YNH_COMPOSER_VERSION="2.2.7"
+YNH_COMPOSER_VERSION="2.3.3"
 
 pkg_dependencies="php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-pdo php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-xsl php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mysql"
 
