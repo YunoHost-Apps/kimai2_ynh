f11c7df64891bb9128b868bb3b8f4c3630e9d94f
yalh76 <yalh@yahoo.com>
2022-10-19 01:42:51 +0200

    Upgrade to 1.26.0~ynh1

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,11 +3,18 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
+# PHP APP SPECIFIC
+#=================================================
 
-# dependencies used by the app
-YNH_PHP_VERSION="7.4"
-pkg_dependencies="php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-pdo php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-xsl php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-sqlite3"
-YNH_COMPOSER_VERSION="2.3.3"
+YNH_PHP_VERSION=7.4
+
+YNH_COMPOSER_VERSION=2.3.3
+
+php_dependencies="php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-pdo php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-xsl php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-sqlite3"
+
+
+# dependencies used by the app (must be on a single line)
+pkg_dependencies="$php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
