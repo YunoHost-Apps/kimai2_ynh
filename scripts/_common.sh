3aa744e83ca2173e805ea22e37f8f60c92c2c3f7
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-09-09 11:00:59 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,15 +6,15 @@
 # PHP APP SPECIFIC
 #=================================================
 
-YNH_PHP_VERSION=7.4
+#REMOVEME? YNH_PHP_VERSION=7.4
 
 YNH_COMPOSER_VERSION=2.3.3
 
-php_dependencies="php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-pdo php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-xsl php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-sqlite3"
+#REMOVEME? php_dependencies="php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-pdo php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-xsl php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-sqlite3"
 
 
 # dependencies used by the app (must be on a single line)
-pkg_dependencies="$php_dependencies"
+#REMOVEME? pkg_dependencies="$php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
