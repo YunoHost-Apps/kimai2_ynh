9bd1374c79a0a328b6aabb5bb734e5fc8fd5ed38
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-09-09 11:14:15 +0200

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,15 +6,7 @@
 # PHP APP SPECIFIC
 #=================================================
 
-#REMOVEME? YNH_PHP_VERSION=7.4
-
-YNH_COMPOSER_VERSION=2.3.3
-
-#REMOVEME? php_dependencies="php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-pdo php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-xsl php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-sqlite3"
-
-
-# dependencies used by the app (must be on a single line)
-#REMOVEME? pkg_dependencies="$php_dependencies"
+YNH_COMPOSER_VERSION=2.5.8
 
 #=================================================
 # PERSONAL HELPERS
