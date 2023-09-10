26b1b8c61828fe7b0426280f8eadbe54631289bd
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-09-10 08:22:58 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,7 +8,7 @@
 
 YNH_PHP_VERSION=8.2
 
-YNH_COMPOSER_VERSION=2.3.3
+YNH_COMPOSER_VERSION=2.5.8
 
 php_dependencies="php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-pdo php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-xsl php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-sqlite3"
 
