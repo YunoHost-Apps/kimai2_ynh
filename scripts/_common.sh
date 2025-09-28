6e0a5a0dc0762ecb076f7d21aedd12a7774a30fa
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-09-28 14:20:20 +0200

    Testing (#150)
    * Upgrade to v2.34.0 (#137)
    
    * Upgrade sources
    - `main` v2.34.0: https://github.com/kevinpapst/kimai2/releases/tag/2.34.0
    
    * Auto-update READMEs
    
    * Upgrade to v2.35.1 (#140)
    
    * Upgrade sources
    - `main` v2.35.1: https://github.com/kevinpapst/kimai2/releases/tag/2.35.1
    
    * Auto-update READMEs
    
    * Upgrade to v2.36.0 (#142)
    
    * Upgrade sources
    - `main` v2.36.0: https://github.com/kevinpapst/kimai2/releases/tag/2.36.0
    
    * Auto-update READMEs
    
    * Upgrade to v2.36.1 (#143)
    
    * Upgrade sources
    - `main` v2.36.1: https://github.com/kevinpapst/kimai2/releases/tag/2.36.1
    
    * Auto-update READMEs
    
    * Upgrade to v2.37.0 (#145)
    
    * Upgrade sources
    - `main` v2.37.0: https://github.com/kevinpapst/kimai2/releases/tag/2.37.0
    
    * Auto-update READMEs
    
    * Upgrade to v2.38.0 (#147)
    
    * Upgrade sources
    - `main` v2.38.0: https://github.com/kevinpapst/kimai2/releases/tag/2.38.0
    
    * Auto-update READMEs
    
    * Upgrade to v2.40.0 (#149)
    
    * Upgrade sources
    - `main` v2.40.0: https://github.com/kevinpapst/kimai2/releases/tag/2.40.0
    
    * Auto-update READMEs
    
    * [autopatch] Automatic patch to use nodejs/ruby/go/compose resources + remove empty _common.sh (#148)
    
    * Testing (#146)
    
    * Upgrade to v2.34.0 (#137)
    
    * Upgrade sources
    - `main` v2.34.0: https://github.com/kevinpapst/kimai2/releases/tag/2.34.0
    
    * Auto-update READMEs
    
    * Upgrade to v2.35.1 (#140)
    
    * Upgrade sources
    - `main` v2.35.1: https://github.com/kevinpapst/kimai2/releases/tag/2.35.1
    
    * Auto-update READMEs
    
    * Upgrade to v2.36.0 (#142)
    
    * Upgrade sources
    - `main` v2.36.0: https://github.com/kevinpapst/kimai2/releases/tag/2.36.0
    
    * Auto-update READMEs
    
    * Upgrade to v2.36.1 (#143)
    
    * Upgrade sources
    - `main` v2.36.1: https://github.com/kevinpapst/kimai2/releases/tag/2.36.1
    
    * Auto-update READMEs
    
    * Upgrade to v2.37.0 (#145)
    
    * Upgrade sources
    - `main` v2.37.0: https://github.com/kevinpapst/kimai2/releases/tag/2.37.0
    
    * Auto-update READMEs
    
    * Upgrade to v2.38.0 (#147)
    
    * Upgrade sources
    - `main` v2.38.0: https://github.com/kevinpapst/kimai2/releases/tag/2.38.0
    
    * Auto-update READMEs
    
    ---------
    
    Co-authored-by: YunoHost Bot <yunohost-bot@users.noreply.github.com>
    
    * [autopatch] Automatic patch to use nodejs/ruby/go/compose resources + remove empty _common.sh
    
    ---------
    
    Co-authored-by: eric_G <46165813+ericgaspar@users.noreply.github.com>
    Co-authored-by: Yunohost-Bot <>
    
    * cleaning
    
    * Update upgrade
    
    ---------
    
    Co-authored-by: YunoHost Bot <yunohost-bot@users.noreply.github.com>

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,8 +4,6 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-composer_version=2.3.3
-
 boolstr() {
     BOOLSTR=( false true )
     echo "${BOOLSTR[$1]}"
