3c54f894cc9e3262d1493c1588615d9ebe09066b
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-09-28 12:38:19 +0200

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,8 +4,6 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-#REMOVEME? composer_version=2.3.3
-
 boolstr() {
     BOOLSTR=( false true )
     echo "${BOOLSTR[$1]}"
